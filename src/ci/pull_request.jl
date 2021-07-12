4288e22191a208a3dd225381942699d0cdfa57ea
Alexandr Romanenko <waralex@gmail.com>
2021-07-12 12:31:21 +0300

    in work

--- src/ci/pull_request.jl
+++ src/ci/pull_request.jl
@@ -2,10 +2,13 @@ function pull_request_ci(;env = ENV)
     println("is pull request ", is_pull_request(env = env))
     println("pull request number ", pull_request_number(env = env))
     pr_num = pull_request_number(env = env)
+    println("000000")
     pr = get_pull_request(pr_num)
+    println("+++++")
     repo = gh_retry() do
         GitHub.repo(RECIPE_REGISTY)
     end
+    println("!!!!!!")
     files = get_changed_filenames(repo, pr)
     println(files)
 end
