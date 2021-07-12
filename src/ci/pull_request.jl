35f6afe5a8ffe917ad415d25b6975e97191037e3
Alexandr Romanenko <waralex@gmail.com>
2021-07-12 12:21:09 +0300

    in work

--- src/ci/pull_request.jl
+++ src/ci/pull_request.jl
@@ -1,7 +1,6 @@
 function pull_request_ci(;env = ENV)
     println("is pull request ", is_pull_request(env = env))
     println("pull request number ", pull_request_number(env = env))
-    println("get_changed_filenames ", get_changed_filenames(env = env))
     pr_num = pull_request_number(env = env)
     pr = git_pull_request(pr_num)
     repo = gh_retry() do
