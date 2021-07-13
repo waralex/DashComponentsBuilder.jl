1628e8efcda20c50fc7f9a542e972eaba406942e
Alexandr Romanenko <waralex@gmail.com>
2021-07-13 23:22:58 +0300

    fix

--- src/ci/actions.jl
+++ src/ci/actions.jl
@@ -25,7 +25,7 @@ function check_build(pr::CIPullRequest)
     @info "trying to build package..."
     ENV["BUILDER"] = "local"
     recipe = load_recipe(joinpath(pr.pr_repo_dir, pr.pkg_name))
-    build(recipe, verbose = true)
+    build(recipe, verbose = false)
 end
 
 function set_labels(pr::CIPullRequest)
